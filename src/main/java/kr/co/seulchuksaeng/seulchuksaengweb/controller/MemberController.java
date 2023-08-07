4e01f181708b705b8a42952210d989b36e1e03fa
JJONG <joshua_0621@naver.com>
2023-08-07 22:37:51 +0900

    [DEV] 회원가입 EndPoint 작성

--- src/main/java/kr/co/seulchuksaeng/seulchuksaengweb/controller/MemberController.java
+++ src/main/java/kr/co/seulchuksaeng/seulchuksaengweb/controller/MemberController.java
@@ -1,13 +1,32 @@
 package kr.co.seulchuksaeng.seulchuksaengweb.controller;
 
+import kr.co.seulchuksaeng.seulchuksaengweb.domain.dto.form.JoinForm;
+import kr.co.seulchuksaeng.seulchuksaengweb.domain.dto.result.JoinResult;
+import kr.co.seulchuksaeng.seulchuksaengweb.service.MemberService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
 @Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/member")
 public class MemberController {
 
+    private final MemberService memberService;
+
+    @PostMapping("/join")
+    public JoinResult join(JoinForm joinForm) {
+
+        try {
+            Long joinId = memberService.join(joinForm);
+        } catch (IllegalStateException e) {
+            return new JoinResult("fail", e.getMessage());
+        }
+
+        return new JoinResult("success", "회원가입 성공");
+    }
 
 }
