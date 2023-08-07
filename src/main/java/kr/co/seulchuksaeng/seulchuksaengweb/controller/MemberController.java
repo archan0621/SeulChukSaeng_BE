d78ac6e87e2a98ea4143f49d471a91bac087e73d
JJONG <joshua_0621@naver.com>
2023-08-07 15:29:15 +0900

    [DEV] 회원가입 EndPoint 생성

--- src/main/java/kr/co/seulchuksaeng/seulchuksaengweb/controller/MemberController.java
+++ src/main/java/kr/co/seulchuksaeng/seulchuksaengweb/controller/MemberController.java
@@ -1,4 +1,19 @@
 package kr.co.seulchuksaeng.seulchuksaengweb.controller;
 
+import kr.co.seulchuksaeng.seulchuksaengweb.controller.form.MemberForm;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Slf4j
 public class MemberController {
+
+    @GetMapping("/members/join")
+    public String createForm(Model model) {
+        model.addAttribute("memberForm", new MemberForm());
+        return "members/join";
+    }
+
 }
