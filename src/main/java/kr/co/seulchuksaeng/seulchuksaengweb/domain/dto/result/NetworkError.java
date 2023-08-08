75f67a52833402a4947454e002f216bea522b8f1
JJONG <joshua_0621@naver.com>
2023-08-08 21:08:36 +0900

    [DEV] 네트워크 에러 변환 폼 생성
    왜 굳이 같은 DTO를 여러개 만드냐, 추후 toString등 상황에 맞게 확장 가능성도 있고 클래스명을 다 다르게 해야지 코드 가독성이 좋기 때문, 성능 떨어지진 않잖아요?

---
+++ src/main/java/kr/co/seulchuksaeng/seulchuksaengweb/domain/dto/result/NetworkError.java
@@ -0,0 +1,17 @@
+package kr.co.seulchuksaeng.seulchuksaengweb.domain.dto.result;
+
+public class NetworkError {
+    private String result;
+
+    private String message;
+
+    public NetworkError(String result, String message) {
+        this.result = result;
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "{\"result\":\"" + result + "\",\"message\":\"" + message + "\"}";
+    }
+}
