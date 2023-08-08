fced36f0fdbdd0d1efec27b22f8a376c0ec58d54
JJONG <joshua_0621@naver.com>
2023-08-08 16:00:51 +0900

    [DEV] JWT 검증 필터 추가

--- src/main/java/kr/co/seulchuksaeng/seulchuksaengweb/security/SecurityConfig.java
+++ src/main/java/kr/co/seulchuksaeng/seulchuksaengweb/security/SecurityConfig.java
@@ -1,21 +1,26 @@
 package kr.co.seulchuksaeng.seulchuksaengweb.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.parameters.P;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
+@EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final String[] allowedUrls = {"/member/**"}; // 허용 URL 목록
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
-        final String[] allowedUrls = {"/member/**"}; // 허용 URL 목록
-
         return http
                 .csrf(AbstractHttpConfigurer::disable)	// csrf 비활성화
                 .authorizeHttpRequests(requests ->
@@ -25,6 +30,7 @@ public class SecurityConfig {
                 .sessionManagement(sessionManagement ->
                         sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )	// 세션을 사용하지 않으므로 STATELESS 설정
+                .addFilterBefore(jwtAuthenticationFilter, BasicAuthenticationFilter.class) // JWT 검증 필터를 UsernamePasswordAuthenticationFilter 전에 넣음
                 .build();
     }
 }
