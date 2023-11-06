2c656a5f79c0f4963af163e114526dde7caaaff0
JJONG <joshua_0621@naver.com>
2023-11-06 21:55:20 +0900

    [Feat] 참여 인원 있는 경기 성별 수정 불가 및 경기 조회시 출석, 납부 상황 리턴

---
+++ src/main/java/kr/co/seulchuksaeng/seulchuksaengweb/exception/event/ParticipantGenderChangeException.java
@@ -0,0 +1,5 @@
+package kr.co.seulchuksaeng.seulchuksaengweb.exception.event;
+
+public class ParticipantGenderChangeException extends RuntimeException {
+    public ParticipantGenderChangeException() { super("경기에 참여한 인원이 있어서 성별을 변경할 수 없습니다! "); }
+}
