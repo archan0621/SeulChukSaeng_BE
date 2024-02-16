a6ed5c5a5f66a1d414038c76fdb12479bee4c5cc
JJONG <joshua_0621@naver.com>
2024-02-16 15:56:14 +0900

    [FEAT] 예외처리중 토큰 없는 예외 추가

--- src/main/java/kr/co/seulchuksaeng/seulchuksaengweb/exception/GlobalExceptionHandler.java
+++ src/main/java/kr/co/seulchuksaeng/seulchuksaengweb/exception/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import com.github.archan0621.Scope;
 import kr.co.seulchuksaeng.seulchuksaengweb.exception.event.EventException;
 import kr.co.seulchuksaeng.seulchuksaengweb.exception.member.MemberException;
 import kr.co.seulchuksaeng.seulchuksaengweb.exception.security.ExpiredTokenException;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -22,9 +23,17 @@ public class GlobalExceptionHandler {
         return new BasicErrorResult("fail", e.getMessage());
     }
 
+    @ExceptionHandler(AccessDeniedException.class)
+    protected BasicErrorResult handleNoTokenException(Exception e) {
+        return new BasicErrorResult("fail", "개발자에게 전하는 말 - 토큰과 함께 요청하세요");
+    }
+
+
     @ExceptionHandler(RuntimeException.class)
     protected BasicErrorResult handleRunTimeException(Exception e) {
         discordLogger.send(String.format("🚨경고🚨 백엔드에서 예상치 못한 오류 생김 개발한 사람 나와, 오류 내용\n- %s", e.toString()), Scope.here);
+        e.printStackTrace();
         return new BasicErrorResult("fail", "예상치 못한 오류 발생 관리자에게 문의하세요.");
     }
+
 }
